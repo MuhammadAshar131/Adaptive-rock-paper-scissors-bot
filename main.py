b9099444149c5d3960db964cf0942977a24e61ba
Muhammad Ashar <159193964+MuhammadAshar131@users.noreply.github.com>
2026-08-27 00:46:12 +0500

    Remove comment from main.py
    Remove development entrypoint comment from main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
