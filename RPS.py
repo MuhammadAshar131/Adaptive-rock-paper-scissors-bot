40a66b2ea2bdc38db168275579913d80a1e5de82
MuhammadAshar131 <159193964+MuhammadAshar131@users.noreply.github.com>
2026-08-26 18:03:39 +0000

    Add adaptive Rock-Paper-Scissors bot

--- RPS.py
+++ RPS.py
@@ -1,10 +1,37 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponentHistory=[], myHistory=[]):
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if not prev_play:
+        opponentHistory.clear()
+        myHistory.clear()
+        return "R"
+        
+    opponentHistory.append(prev_play)
+    
+    if len(opponentHistory) > len(myHistory):
+        myHistory.append("R") 
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    combinedPairs = [f"{m}{o}" for m, o in zip(myHistory, opponentHistory)]
+    
+    n = 2
+    if len(combinedPairs) < n:
+        myHistory.append("P")
+        return "P"
 
-    return guess
+    currentContext = "".join(combinedPairs[-n:])
+    predictionCounts = {"R": 0, "P": 0, "S": 0}
+
+    for i in range(len(combinedPairs) - n):
+        context = "".join(combinedPairs[i : i + n])
+        if context == currentContext:
+            nextOppMove = opponentHistory[i + n]
+            predictionCounts[nextOppMove] += 1
+            
+    predictedMove = max(predictionCounts, key=predictionCounts.get)
+    if predictionCounts[predictedMove] == 0:
+        predictedMove = max(set(opponentHistory), key=opponentHistory.count)
+
+    idealResponse = {"R": "P", "P": "S", "S": "R"}
+    nextMove = idealResponse[predictedMove]
+    
+    myHistory.append(nextMove)
+    return nextMove
